840758e312bef775379250ad33e603a630e14462
tomaspolski90-creator <tomaspolski90@gmail.com>
2026-02-13 10:21:51 +0100

    Update cron.js

--- backend/cron.js
+++ backend/cron.js
@@ -1,22 +1,42 @@
 // cron.js - Scheduled tasks for Trackisto
 const shopifyRoutes = require('./routes/shopify');
+const woocommerceRoutes = require('./routes/woocommerce');
 
 const CRON_INTERVAL = 30 * 60 * 1000; // 30 minutes
+
+async function runAutoFulfillment() {
+  console.log('[Cron] Running auto-fulfillment for all store types...');
+  
+  try {
+    await shopifyRoutes.processAutoFulfillment();
+    console.log('[Cron] Shopify auto-fulfillment completed');
+  } catch (err) {
+    console.error('[Cron] Shopify auto-fulfillment error:', err.message);
+  }
+  
+  try {
+    console.log('[Cron] Starting WooCommerce auto-fulfillment...');
+    await woocommerceRoutes.processWooCommerceAutoFulfillment();
+    console.log('[Cron] WooCommerce auto-fulfillment completed');
+  } catch (err) {
+    console.error('[Cron] WooCommerce auto-fulfillment error:', err.message);
+  }
+}
 
 function startCronJobs() {
   console.log('[Cron] Starting scheduled tasks...');
   
   setTimeout(() => {
     console.log('[Cron] Running initial auto-fulfillment check...');
-    shopifyRoutes.processAutoFulfillment();
+    runAutoFulfillment();
   }, 10000);
   
   setInterval(() => {
     console.log('[Cron] Running scheduled auto-fulfillment...');
-    shopifyRoutes.processAutoFulfillment();
+    runAutoFulfillment();
   }, CRON_INTERVAL);
   
-  console.log('[Cron] Auto-fulfillment scheduled to run every 30 minutes');
+  console.log('[Cron] Auto-fulfillment scheduled to run every 30 minutes (Shopify + WooCommerce)');
 }
 
 module.exports = { startCronJobs };
